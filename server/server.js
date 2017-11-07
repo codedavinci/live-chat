564c9ebf4ae091816e24f3a60ab945cd47f63478
mac <codedavinci.now@gmail.com>
2017-11-07 01:15:14 -0800

    Connect and disconnect event emmiters

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path')
 const express = require("express")
+const http = require('http')
+const socketIO = require('socket.io')
 
 const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '../client/public' )
 const app = express()
 
+const server = http.createServer(app)
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  socket.on('disconnect', () => {
+  console.log('User was disconnected')
+})
+})
+
 app.use(express.static(publicPath))
 
-app.listen(port, () => console.log("Server is up on port: ", port))
+server.listen(port, () => console.log("Server is up on port: ", port))
 
 
